0837c7f5bd8a8f2a26ff4458bdae5b68981dc135
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-03-13 11:09:23 +0000

    Workaround for typing into editor with placeholder

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-import {Selector} from 'testcafe'
+import {Selector, ClientFunction} from 'testcafe'
 
 fixture('ProseMirror repro').page('http://localhost:8080')
 
@@ -13,3 +13,22 @@ test('empty editor with placeholder', t => {
   return t.typeText(selector, 'Hello test')
       .expect(selector.innerText).contains('Hello')
 })
+
+test('workaround for empty editor with placeholder', async t => {
+  const selector = Selector('#with-placeholder div[contenteditable]')
+  await t.typeText(...await prepareEditor(selector, 'Hello test'))
+  	.expect(selector.innerText).contains('Hello')
+})
+
+async function prepareEditor(selector, text) {
+	const hasPlaceholder = await selector.find('.placeholder').exists
+	const options = {}
+	if (hasPlaceholder) {
+		await goToEditMode(selector.child())
+		options.replace = true
+	}
+	return [selector, text, options]
+}
+
+
+const goToEditMode = ClientFunction(selector => selector().innerHTML += '')
